3f5f4ed54ca67c95b759699b5bb70a5fd2379bf2
Your Name <you@example.com>
2019-09-02 10:58:47 -0700

    joi

--- src/components/register/login.jsx
+++ src/components/register/login.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import PropTypes from 'prop-types'; 
+import Joi from 'joi-browser'
 
 export default class Login extends Component {
     state = {
@@ -9,8 +10,14 @@ export default class Login extends Component {
             us: '',
             ps: ''
         }, 
-        token: ''
+        token: '',
+        error: []
     }
+
+    schema = Joi.object().keys({
+        us: Joi.string().alphanum().min(5).max(30).required(),
+        ps: Joi.string().regex(/^[a-zA-Z0-9]{5,30}$/).required()
+    }).with('us', 'ps')
 
     handleInput = (e) => {
         let state = {...this.state}
@@ -63,18 +70,49 @@ export default class Login extends Component {
           });          
     }
 
+    validate = () =>{
+        let err = []
+        let {us, ps} = this.state.inputs
+        if(us.trim().length == 0){
+            err = [...err, 'username is requerd']
+        }
+        if(ps.trim().length == 0){
+            err = [...err, 'password is requerd']
+        }
+        if(us.trim().length < 5){
+            err = [...err, 'username is to smoll']
+        }
+        if(ps.trim().length < 5){
+            err = [...err, 'password is to smoll']
+        }
+        console.log(err)
+        this.setState({error: [...err]})
+        return err.length == 0
+    }
+
+    validateJoi = () => {
+        const result = Joi.validate({...this.state.inputs}, this.schema, { abortEarly: false });
+        let arr = result.error.details.map(item => item.message)
+        this.setState({error: [...arr]})
+        //console.log(result.error.details[0].message)
+    }
+
     handleSubmit = async (e) => {
         e.preventDefault()
         //this.fetchGet('http://localhost:3001/api/')
         //this.axiosGet('http://localhost:3001/api/',{})
         //send server request
         //this.fetchPost('http://localhost:3001/api/', this.state.inputs)
-        let data = await this.fetchPost('http://localhost:3001/api/login', this.state.inputs)
-        //this.setState({token: data})
-        if(data.error)
-            console.log(data.error)
-        else
-            this.setState({token: data})
+        this.validateJoi()
+        /*let errr = this.validate()
+        if(!errr){
+            let data = await this.fetchPost('http://localhost:3001/api/login', this.state.inputs)
+            //this.setState({token: data})
+            if(data.error)
+                console.log(data.error)
+            else
+                this.setState({token: data})
+        }*/
     }
     handleSubmitAdmin = async (e) => {
         e.preventDefault()
@@ -96,6 +134,11 @@ export default class Login extends Component {
                     <input type="text" name="ps" placeholder="Password" onChange={this.handleInput} value={this.state.inputs.ps} /><br />
                     <button>send</button>
                 </form>
+                <div>
+                {this.state.error.map(item => {
+                    return <div key={item} className="err">{item}</div>
+                })}
+                </div>
                 <form onSubmit={this.handleSubmitAdmin}>
                     <input type="hidden" name="token" placeholder="token" value={this.state.token} /><br />
                     <button>send</button>
